9ac9896c27054e91d88e9bb477caa3d3fccbc5c9
joetta Hall <joetta.chasidy@gmail.com>
2019-01-22 00:04:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
      "Fizz"
-  if int % 5 == 0
+  elsif int % 5 == 0
      "Buzz"
-  if int % 3 && 5 == 0
+  else int % 3 && 5 == 0
      "FizzBuzz"
   end
 end
